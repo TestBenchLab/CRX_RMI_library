21cbab010f22d25830091db0bf3b8ca378713bc7
victorgaudin99 <victor.gaudin@alten.com>
2024-12-18 14:53:38 +0100

    minor change

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(
     name='rmi_library',              
     version='0.1',                    
     packages=find_packages(),         # Find-submodules
-    install_requires=[                # Dependencies
-        # Exemple : 'numpy>=1.19'
+    install_requires=[                # External Dependencies
     ],
     author='Valentin Deguil / Victor Gaudin',
     description='RMI Library to send robot commands through TCP/IP',
-    url='https://github.com/TODO',
+    url='https://github.com/ModularAutomatedRailsSplicing/CRX_RMI_library/tree/standalone',
 )

7354a18737755bf98a012792c756a5a02b4afa88
clgp44 <clgpoulain@gmail.com>
2026-02-11 11:03:35 +0100

    rmi_set_payload fix

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="rmi_library",
-    version="0.2",
+    version="1.0",
     packages=find_packages(),  # Find-submodules
     install_requires=[  # External Dependencies
         "numpy>=1.22.4",
     ],
-    author="Valentin Deguil / Victor Gaudin",
+    author="Valentin Deguil / Victor Gaudin / Christophe Poulain",
     description="RMI Library to send robot commands through TCP/IP",
-    url="https://github.com/ModularAutomatedRailsSplicing/CRX_RMI_library/tree/standalone",
+    url="https://github.com/TestBenchLab/CRX_RMI_library/tree/standalone",
 )
